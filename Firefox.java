af0da12d7396da9786760c0c38d78d667fbfb0f8
AnuraagSeenuvasan <112798792+AnuraagSeenuvasan@users.noreply.github.com>
2022-09-04 12:35:12 +0430

    Add files via upload

---
+++ Firefox.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float version=100.2f;
+		String name="firefox";
+		boolean visible=true;
+		int year=1998;
+		char logo='m';
+		
+
+		System.out.print(version);
+		System.out.print(name);
+		System.out.println(visible);
+		System.out.println(year);
+		System.out.println(logo);
+	}
+
+}
---
+++ OddOrEven.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class OddOrEven {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number=25;
+		if(number%2==0) {
+			System.out.println("The given input is Even");
+		}
+		else {
+			System.out.println("The given input is Odd");
+		}
+	}
+
+}
